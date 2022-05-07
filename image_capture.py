bfe55fa0f1c65872352960d618a918cc4df8672b
unknown <gregoriazuaje04@gmail.com>
2022-05-06 20:23:54 -0400

    finished GUI and transformed the image capture functions into a threading class to run the program and the GUI doesn't get freeze

--- image_capture.py
+++ image_capture.py
@@ -1,44 +1,77 @@
-from imagesearch import imagesearch, region_grabber
 import cv2
 import pytesseract
 import numpy as np
 import re
+import threading
+import time
+import chime
+from imagesearch import imagesearch, region_grabber
 
 
-def set_coordinates_percentage_area(loc_coordinates_upleft):
-    x_left_cord = loc_coordinates_upleft[0] + 150
-    y_up_cord = loc_coordinates_upleft[1] + 15
-    x_right_cord = loc_coordinates_upleft[0] + 250
-    y_down_cord = loc_coordinates_upleft[1] + 40
-    return (x_left_cord, y_up_cord, x_right_cord, y_down_cord)
+class DetectScreen(threading.Thread):
 
+    def __init__(self, query_percentage, more_or_less, info_window_widget):
+                super().__init__()
+                self.query_percentage = query_percentage
+                self.more_or_less = more_or_less
+                self.info_window = info_window_widget
+                self.detener = threading.Event()
+                self.daemon = True
+                chime.theme("zelda")
+                pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
-# get grayscale image to improve tesseract output
-def get_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    def run(self):
+        while True:
+            if self.detener.is_set():
+                return
+            self.locate_portfolio_section()
+            time.sleep(2)
 
+    def set_coordinates_percentage_area(self, loc_coordinates_upleft):
+        x_left_cord = loc_coordinates_upleft[0] + 150
+        y_up_cord = loc_coordinates_upleft[1] + 15
+        x_right_cord = loc_coordinates_upleft[0] + 250
+        y_down_cord = loc_coordinates_upleft[1] + 40
+        return (x_left_cord, y_up_cord, x_right_cord, y_down_cord)
 
-def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    # get grayscale image to improve tesseract output
+    def get_grayscale(self, image):
+        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    def thresholding(self, image):
+        return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-def process_image(image):
-    img = cv2.resize(image, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
-    img = get_grayscale(img)
-    kernel = np.ones((1, 1), np.uint8)    
-    img = cv2.dilate(img, kernel, iterations=1)    
-    img = cv2.erode(img, kernel, iterations=1)
-    img = thresholding(img)
-    return img
+    def process_image(self, image):
+        img = cv2.resize(image, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+        img = self.get_grayscale(img)
+        kernel = np.ones((1, 1), np.uint8)    
+        img = cv2.dilate(img, kernel, iterations=1)    
+        img = cv2.erode(img, kernel, iterations=1)
+        img = self.thresholding(img)
+        return img
 
+    def locate_portfolio_section(self):
+        query_percentage = int(self.query_percentage)
+        loc_coordinates_upleft = imagesearch("images/ganancia-actual-big-size-font.png")
 
-def locate_portfolio_section(query_percentage):
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
-    loc_coordinates_upleft = imagesearch("images/ganancia-actual-big-size-font.png")
-    coordinates_percentage_area = set_coordinates_percentage_area(loc_coordinates_upleft)
-    profit_section = region_grabber(coordinates_percentage_area)
-    profit_section.save("images/screenshot.png")
-    percentage_img = cv2.imread('images/screenshot.png')
-    percentage_img = process_image(percentage_img)
-    result = pytesseract.image_to_string(percentage_img, lang="eng")
-    percentage_in_screen = int(re.search(r'[+ -]+\d+', result).group())
+        if loc_coordinates_upleft is None:
+            return
+
+        coordinates_percentage_area = self.set_coordinates_percentage_area(loc_coordinates_upleft)
+        profit_section = region_grabber(coordinates_percentage_area)
+        profit_section.save("images/screenshot.png")
+        percentage_img = cv2.imread('images/screenshot.png')
+        percentage_img = self.process_image(percentage_img)
+        result = pytesseract.image_to_string(percentage_img, lang="eng")
+        self.info_window.insert_text(f"El porcentaje de ganancia actual es: {result}")
+        percentage_in_screen = int(re.search(r'[+ — -]\d+', result).group().replace("—", "-"))
+
+        if self.more_or_less == "Mayor a":
+            if percentage_in_screen >= query_percentage:
+                chime.success()
+        if self.more_or_less == "Menor a":
+            if percentage_in_screen <= query_percentage:
+                chime.success()
+
+    def stop(self):
+        self.detener.set()
--- main.py
+++ main.py
@@ -1,37 +1,99 @@
-from image_capture import locate_portfolio_section
+from image_capture import DetectScreen
+from tkinter import messagebox
+from tkinter import scrolledtext
 import tkinter as tk
+import re
+
+
+"""customized scrolled text area class to define method for insert text in a new line everytime"""
+class TextareaInfoWidget(scrolledtext.ScrolledText):
+
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.last_line_counter = 1.0
+
+    def insert_text(self, text):
+        self.config(state='normal')
+        self.insert(str(self.last_line_counter), text + "\n")
+        self.config(state='disabled')
+        self.last_line_counter += 1.0
 
 
 class MainFrame(tk.Frame):
 
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.config(padx=50, pady=20)
         vldt_ifnum_cmd = (self.register(self.validate_percentage_number), '%S')
-        percentage_limit = tk.Spinbox(self, from_=-100, to=100, width=10, justify="center", validate="all", validatecommand=vldt_ifnum_cmd)
-        percentage_limit.insert(0, 0)
-        percentage_limit.grid(row=0, column=0)
-        initiate_button = tk.Button(
+        self.program_active = None  # Flag to check if the thread for image_capture is running
+        self.percentage_limit_search = tk.LabelFrame(self, text="Introduzca porcentaje de ganancia deseado:", pady=5, padx=5)
+        options_list = ["Mayor a", "Menor a"]  # List of options for OptionMenu
+        self.value_option_menu = tk.StringVar(self.percentage_limit_search)
+        self.bigger_less_than_option = tk.ttk.OptionMenu(self.percentage_limit_search, self.value_option_menu, options_list[0], *options_list)
+        self.percentage_sign = tk.Label(self.percentage_limit_search, text="%")
+        self.percentage_limit = tk.Spinbox(self.percentage_limit_search, from_=-100, to=100, width=10, justify="center", validate="all", validatecommand=vldt_ifnum_cmd)
+        self.percentage_limit.insert("0", "0")
+        self.start_button = tk.Button(
             self, 
-            text="Iniciar", 
-            command=lambda: locate_portfolio_section(percentage_limit.get()),
-            bg="green", 
-            pady=5, 
-            padx=10, 
-            font={"size": 20}
+            text = "Iniciar", 
+            command = self.start_program,
+            bg = "#84e73f", 
+            pady = 5, 
+            padx = 10,
+            state = "normal",
+            font = {"size": 20}
         )
-        initiate_button.grid(row=0, column=1, padx=10)
+        self.info_window = TextareaInfoWidget(self, width=40, height=10, 
+                                                        state='disabled', wrap='word')
+        
+        # Elements for the self frame
+        self.percentage_limit_search.grid(row=0, column=0)
+        self.start_button.grid(row=0, column=1, padx=10)
+        self.info_window.grid(row=1, column=0, columnspan=2, pady=10)
+
+        # Elements for percentage_limit_search LabelFrame
+        self.bigger_less_than_option.grid(row=0, column=0, padx=5)
+        self.percentage_limit.grid(row=0, column=1, padx=5)
+        self.percentage_sign.grid(row=0, column=2)
+        
+        
 
     def validate_percentage_number(self, new_value):
         """Check the value inserted in percentage spinbox are only numbers"""
-        valid = new_value == '' or new_value.isdigit() or new_value == '-'
-        if not valid:
+        match = re.match(r'[\d -]+', new_value)
+        valid = new_value == ''
+        if match is None and (not valid):
             self.bell()
-        return valid
+            return False
+        else:
+            return True
+
+    def start_program(self):
+        if self.program_active is None:
+            if not self.percentage_limit.get().lstrip("-").isdigit():
+                messagebox.showerror("Error en el porcentaje deseado", 
+                                    "Por favor introduzca un dígito válido en el cuadro del porcentaje deseado")
+                return
+            self.percentage_limit.config(state="disabled")
+            self.bigger_less_than_option.configure(state="disabled")
+            self.program_active = DetectScreen(self.percentage_limit.get(), self.value_option_menu.get(), self.info_window)
+            self.program_active.start()
+            self.info_window.insert_text("Iniciando, mantenga la app de Iqoption desplegada en pantalla...")
+            self.start_button["bg"] = "#ff4949"
+            self.start_button["text"] = "Detener"
+        else:
+            self.program_active.stop()
+            self.start_button["bg"] = "#84e73f"
+            self.start_button["text"] = "Iniciar"
+            self.percentage_limit.config(state="normal")
+            self.bigger_less_than_option.configure(state="normal")
+            self.program_active = None
         
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title = "Lector porcentaje de profit Iqoption"
+    root.title("Lector porcentaje de ganancia Iqoption")
+    root.iconbitmap("images/iqicon.ico")
+    MainFrame(root).pack(expand=True)
     root.resizable(width=False, height=False)
-    MainFrame(root, width=200, height=200, padx=30, pady=30).grid()
     root.mainloop()
